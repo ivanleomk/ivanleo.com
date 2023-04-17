1d0c8ced03b553eb1a111d6747e519d41e96c1b0
Ivan Leo <ivanleo@Ivans-MacBook-Pro.local>
2023-04-17 11:36:46 +0800

    Working build

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
+const { withContentlayer } = require("next-contentlayer");
 
-module.exports = nextConfig
+module.exports = withContentlayer({
+  experimental: { appDir: true },
+});
