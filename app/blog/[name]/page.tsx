bf5493aded1a44add67c2522d0db31b3962fad36
Ivan Leo <ivanleomk@gmail.com>
2023-05-13 18:11:04 +0800

    Fixed up a new fastai page

--- app/blog/[name]/page.tsx
+++ app/blog/[name]/page.tsx
@@ -128,7 +128,9 @@ const BlogPage = async ({ params }: { params: Params }) => {
       </div>
       <hr className="my-8 border-slate-200 w-40 mx-auto" />
       <div className="flex flex-col items-center justify-start lg:flex-row lg:items-start lg:justify-between">
-        <TableOfContents source={post.body.raw} />
+        <div className="max-w-md">
+          <TableOfContents source={post.body.raw} />
+        </div>
 
         <article className="max-w-full px-4 pb-8 text-dark lg:max-w-2xl">
           <Content components={{ ...CodeBlocks, StaticTweet }} />
